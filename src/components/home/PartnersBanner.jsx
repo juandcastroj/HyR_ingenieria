b2dfc7c5cb9026cb465da062f125d824c7cb65e0
Juan David Castro <juandavid4130@gmail.com>
2025-11-05 23:30:27 -0500

    services section

--- src/components/home/PartnersBanner.jsx
+++ src/components/home/PartnersBanner.jsx
@@ -1,13 +1,19 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
 import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import partner1 from "../../assets/images/partners/acueducto_edited.png"
+import partner2 from "../../assets/images/partners/alcaldia bogota_edited.png"
+import partner3 from "../../assets/images/partners/car_edited.png"
+import partner4 from "../../assets/images/partners/consical_edited.png"
+import partner5 from "../../assets/images/partners/ibal_edited.png"
+
 
 export default function PartnersBanner() {
   return (
-    <div className="bg-[#032579] py-8 sm:pt-8">
+    <div className="bg-[#051a50] py-12 sm:pt-8">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h2 className="text-center text-base sm:text-lg/8 font-semibold text-gray-200">
-          Con la confianza de algunos de los equipos más innovadores del país :
+          Algunos de nuestros clientes:
         </h2>
 
         <Swiper
@@ -28,73 +34,84 @@ export default function PartnersBanner() {
         >
           <SwiperSlide className="flex items-center justify-center">
             <img
-              alt="Transistor"
-              src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-white.svg"
-              
-              
-              className="max-h-12 w-full object-contain h-[70px] sm:h-[48px]"
+              alt="Acueducto de Bogotá"
+              src={partner1}
+              className="w-full object-contain h-[70px]"
             />
           </SwiperSlide>
 
           <SwiperSlide className="flex items-center justify-center">
             <img
-              alt="Reform"
-              src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-white.svg"
-              
-              
-              className="max-h-12 w-full object-contain h-[70px] sm:h-[48px]"
+              alt="Alcaldía de Bogotá"
+              src={partner2}
+              className="w-full object-contain h-[70px]"
             />
           </SwiperSlide>
 
           <SwiperSlide className="flex items-center justify-center">
             <img
-              alt="Tuple"
-              src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-white.svg"
-              
-              
-              className="max-h-12 w-full object-contain h-[70px] sm:h-[48px]"
+              alt="Car"
+              src={partner3}
+              className="w-full object-contain h-[70px]"
             />
           </SwiperSlide>
 
           <SwiperSlide className="flex items-center justify-center">
             <img
-              alt="SavvyCal"
-              src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-white.svg"
-              
-              
-              className="max-h-12 w-full object-contain h-[70px] sm:h-[48px]"
+              alt="Consical"
+              src={partner4}
+              className="object-cover h-[70px] w-[160px]"
             />
           </SwiperSlide>
 
           <SwiperSlide className="flex items-center justify-center">
             <img
-              alt="Statamic"
-              src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-white.svg"
-              
-              
-              className="max-h-12 w-full object-contain h-[70px] sm:h-[48px]"
+              alt="Ibal"
+              src={partner5}
+              className="w-full object-contain h-[70px]"
+            />
+          </SwiperSlide>
+          
+          <SwiperSlide className="flex items-center justify-center">
+            <img
+              alt="Acueducto de Bogotá"
+              src={partner1}
+              className="w-full object-contain h-[70px]"
             />
           </SwiperSlide>
 
           <SwiperSlide className="flex items-center justify-center">
             <img
-              alt="Tuple"
-              src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-white.svg"
-              
-              
-              className="max-h-12 w-full object-contain h-[70px] sm:h-[48px]"
+              alt="Alcaldía de Bogotá"
+              src={partner2}
+              className="w-full object-contain h-[70px]"
             />
           </SwiperSlide>
 
           <SwiperSlide className="flex items-center justify-center">
             <img
-              alt="Statamic"
-              src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-white.svg"
-              
-              
-              className="max-h-12 w-full object-contain h-[70px] sm:h-[48px]"
+              alt="Car"
+              src={partner3}
+              className="w-full object-contain h-[70px]"
             />
           </SwiperSlide>
+
+          <SwiperSlide className="flex items-center justify-center">
+            <img
+              alt="Consical"
+              src={partner4}
+              className="object-cover h-[70px] w-[160px]"
+            />
+          </SwiperSlide>
+
+          <SwiperSlide className="flex items-center justify-center">
+            <img
+              alt="Ibal"
+              src={partner5}
+              className="w-full object-contain h-[70px]"
+            />
+          </SwiperSlide>
+
         </Swiper>
       </div>
     </div>
---
+++ src/constants/services.js
@@ -0,0 +1,26 @@
+export const services = [
+  {
+    name: 'TINTAS',
+    description: 'Inspección de soldaduras por método de tintas penetrantes e inspección visual.',
+    attendant: 'INSPECTOR CALIFICADO DE TINTAS',
+    contact: '(+57) 310 348 3588',
+  },
+  {
+    name: 'MONTAJES Y PRUEBAS',
+    description: 'Montajes, reparaciones en tuberías y pruebas hidrostáticas.',
+    attendant: 'CONTRATISTA',
+    contact: '(+57) 311 521 8487',
+  },
+  {
+    name: 'SOLDADURAS',
+    description: 'Fabricación e instalación de accesorios, soportes, piezas especiales y estructuras metálicas.',
+    attendant: 'INGENIERO MECÁNICO',
+    contact: '(+57) 310 348 3588',
+  },
+  { 
+    name: 'CIVIL E HIDRÁULICA',
+    description: 'Obras Civiles, Proyectos de hidráulica.' ,
+    attendant: 'INGENIERO CIVIL',
+    contact: '(+57) 319 268 4463',
+  }
+]
