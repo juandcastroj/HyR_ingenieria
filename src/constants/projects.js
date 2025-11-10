fddef9aed496be702150e5c40a875035ffc00d18
Juan David Castro <juandavid4130@gmail.com>
2025-11-10 13:20:12 -0500

    First projects component

---
+++ src/constants/projects.js
@@ -0,0 +1,24 @@
+import img1 from "../assets/images/projects/ptar/01.png";
+import img2 from "../assets/images/projects/ptar/02.png";
+import img3 from "../assets/images/projects/ptar/03.png";
+import img4 from "../assets/images/projects/ptar/04.png";
+import img5 from "../assets/images/projects/ptar/05.png";
+import img6 from "../assets/images/projects/ptar/06.png";
+import img7 from "../assets/images/projects/ptar/07.png";
+import img8 from "../assets/images/projects/ptar/08.png";
+import img9 from "../assets/images/projects/ptar/09.png";
+import img10 from "../assets/images/projects/ptar/10.png";
+
+
+export const projectsImages = [
+  { id: 1, img: img1, caption: "Nuestros inicios en la recolección de materiales." },
+  { id: 2, img: img2, caption: "Participación en ferias de emprendimiento." },    
+  { id: 3, img: img3, caption: "Talleres de sensibilización ambiental en colegios." },
+  { id: 4, img: img4, caption: "Reuniones de equipo para planificar proyectos." },
+  { id: 5, img: img5, caption: "Colaboración con la comunidad local." },
+  { id: 6, img: img6, caption: "Procesamiento de materiales reciclables." },
+  { id: 7, img: img7, caption: "Producción de nuestros productos sostenibles." },
+  { id: 8, img: img8, caption: "Eventos de concientización y educación ambiental." },
+  { id: 9, img: img9, caption: "Producción de nuestros productos sostenibles." },
+  { id: 10, img: img10, caption: "Eventos de concientización y educación ambiental." },
+];
